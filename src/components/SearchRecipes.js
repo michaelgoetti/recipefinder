a0a7912438cf9f67ba55855bfa2983789473df99
Michael Goetti <mpgoetti at gmail dot com>
2017-06-28 01:12:57 -0500

    test second commit

--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -15,7 +15,7 @@ class SearchRecipes extends Component {
 
 	search() {
 		let { ingredients, dish } = this.state;
-		const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+		const url = `https://cors-everywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
 
 		fetch(url, {
 			method: 'GET'
